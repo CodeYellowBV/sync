d80661e069abd6dd3ba8e6d9c57c20c302ffac86
Stefan <stefan@codeyellow.nl>
2014-09-01 16:43:41 +0200

    Changed proxy

--- src/CodeYellow/Sync/Proxy/Controller/Proxy.php
+++ src/CodeYellow/Sync/Proxy/Controller/Proxy.php
@@ -4,6 +4,7 @@ trait Proxy
 {
     /**
      * Routes a request to the correct url
+     * @param string What to sync
      */
     public function sync($what)
     {
@@ -13,6 +14,20 @@ trait Proxy
             \App::abort(404);
         }
         $server = $config['servers'][$what];
+        $input = file_get_contents('php://input');
+        $this->doRequest($server, $input);
 
+    }
+
+    private function doRequest($server, $input)
+    {
+        try {
+            $client = new \GuzzleHttp\Client();
+            $res = $client->post($server['url'], ['body' => $input]);
+        } catch (\GuzzleHttp\Exception\BadResponseException $e) {
+            \App::abort($e->getResponse()->getStatusCode(), 'An error has occured. URL:' . $server['url']. ' input:' . $input);
+            die;
+        }
+        echo $res->getBody();
     }
 }

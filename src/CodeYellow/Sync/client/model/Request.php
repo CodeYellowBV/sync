7196d1fb7c6ca2f3c840c7a8ff334ec0dcc0862c
Stefan <stefan@codeyellow.nl>
2014-09-01 12:55:45 +0200

    First models

---
+++ src/CodeYellow/Sync/client/model/Request.php
@@ -0,0 +1,94 @@
+<?php
+namespace CodeYellow\Sync\Client\Model;
+class Request
+{
+    const TYPE_NEW = 'new';
+    const TYPE_MODIFIED = 'modified';
+
+    private $type;
+    private $limit;
+    private $before;
+    private $since;
+    private $startId;
+
+    private $result; // The result of this query
+
+    /**
+     * Create a sync. Sets ths json
+     *
+     * @throws Exception\Sync\MalformedJsonException If json is malformed
+     * @throws Exception\Sync\wrongParameterException If json parameters do not meet the specification
+     */
+    public function __construct($json)
+    {
+        $this->readJson($json);
+    }
+
+    /**
+     * Verifies the json. If json is ok -> sets the private vars
+     */
+    protected function readJson($json)
+    {
+        $request = json_decode($json);
+        
+        if ($request == null) {
+            throw new \Exception\Sync\MalformedJsonException();
+        }
+
+        foreach (['limit', 'before', 'since', 'startId'] as $option) {
+            if (!is_int($request->$option) && !is_null($request->$option)) {
+                throw new \Exception\Sync\UnexpectedTypeException($option . ' should be an integer');
+            }
+        }
+
+        // Read type
+        if (!isset($request->type) || !in_array($request->type, [static::TYPE_NEW, static::TYPE_MODIFIED])) {
+            throw new \Exception\Sync\UnexpectedTypeException();
+        }
+
+        $this->type = $request->type;
+        $this->limit = $request->limit;
+        $this->before = $request->before;
+        $this->since = $request->since;
+        $this->startId = $request->startId;
+    }
+
+    /**
+     * Do a sync
+     * 
+     * Use $eloquent->getQuery() to get the query from eloquent
+     * @param Illuminate\Database\Query\Builder $query The prepared query without
+     * @param int $limit The limit for how many results may be exported. If null, use user limit
+     */
+    public function doSync(\Illuminate\Database\Query\Builder $query, $limit = null)
+    {
+        if (!is_int($limit) && !is_null($limit)) {
+            throw new InvalidArgumentException('SyncRequest::doSync limit must be an integer');
+        }
+
+        // Check if we use created_at or updated at
+        $sortOn = $this->type == static::TYPE_NEW ? 'created_at' : 'updated_at';
+        $before = is_null($this->before) ? time() : min(time(), $this->before);
+
+        // Disregard things from before now to ensure no results are lost
+        $query->where($sortOn, '<', $before);
+
+        // Unsynced result are where
+        // (time > now || (time == now && id >= startId))
+        $query->where(function ($query) use ($sortOn) {
+            $query->where($sortOn, '>', $this->since);
+            $query->orWhere(function ($query) use ($sortOn) {
+                $query->where($sortOn, '=', $this->since);
+                $query->where('id', '>=', $this->startId);
+            });
+        });
+
+        // Check if a limit is set, if not, set limit to given limit
+        $count = $query->aggregate('count');
+
+        $limit == null && $limit = $this->limit;
+        $query->limit(min($this->limit, $limit));
+        $this->result = new Result($query->get(), $count);
+        return $this->result;
+    }
+}
---
+++ src/CodeYellow/Sync/client/model/Result.php
@@ -0,0 +1,49 @@
+<?php
+namespace CodeYellow\Sync\Client\Model;
+class Result
+{
+    private $count;
+    private $remaining;
+    private $data;
+
+    /**
+     * Create a new sync result
+     * @param array $data The data to sync
+     * @param int $totalRecords How many records are there in total (remaining + data)
+     */
+    public function __construct(array $data, $totalRecords)
+    {
+        if (!is_int($totalRecords)) {
+            throw new InvalidArgumentException('syncResult: totalRecords must be an integer');
+        }
+
+        $this->count = count($data);
+        $this->remaining = max($totalRecords - $this->count, 0);
+
+        foreach ($data as $key => $result) {
+            $this->data[$key] = (array) $result;
+        }
+    }
+
+    /**
+     * Return an array representation of this object
+     * @return array The result
+     */
+    public function asArray()
+    {
+        return [
+            'count' => $this->count,
+            'remaining' => $this->remaining,
+            'data' => $this->data
+        ];
+    }
+
+    /**
+     * Return a json representation of this object
+     * @return string Json representation of the result
+     */
+    public function asJson()
+    {
+        return json_encode($this->asArray());
+    }
+}

fc2ac64edf887f0dbe06e5c10faba737c3735fba
Stefan <stefan@codeyellow.nl>
2014-09-20 14:00:31 +0200

    More tests

--- src/CodeYellow/Sync/Proxy/Controller/Proxy.php
+++ src/CodeYellow/Sync/Proxy/Controller/Proxy.php
@@ -3,32 +3,49 @@ namespace CodeYellow\Sync\Proxy\Controller;
 trait Proxy
 {
 
+    public function getConfig($item)
+    {
+        return $this->app['config']->get($item);
+    }
+
+    public function getApp()
+    {
+        return $this->app['app'];
+    }
+
+    public function getGuzzle()
+    {
+        return new \GuzzleHttp\Client();
+    }
+
     /**
      * Routes a request to the correct url
      * @param string What to sync
+     * @return string response
      */
     public function sync($what)
     {
-        $config = $this->app['config']->get('packages/codeyellow/sync/config');
+        $config = $this->getConfig('packages/codeyellow/sync/config');
 
         if (!isset($config['servers'][$what])) {
-            \App::abort(404);
+            $this->getApp()->abort(404);
+            return;
         }
         $server = $config['servers'][$what];
         $input = file_get_contents('php://input');
-        $this->doRequest($server, $input);
+        return $this->doRequest($server, $input);
 
     }
 
     private function doRequest($server, $input)
     {
         try {
-            $client = new \GuzzleHttp\Client();
+            $client = $this->getGuzzle();
             $res = $client->post($server['url'], ['body' => $input]);
         } catch (\GuzzleHttp\Exception\BadResponseException $e) {
-            \App::abort($e->getResponse()->getStatusCode(), 'An error has occured. URL:' . $server['url']. ' input:' . $input);
-            die;
+            $this->getApp()->abort($e->getResponse()->getStatusCode(), 'An error has occured. URL:' . $server['url']. ' input:' . $input);
+            return;
         }
-        echo $res->getBody();
+        return $res->getBody();
     }
 }
--- tests/Proxy/Controller/ProxyTest.php
+++ tests/Proxy/Controller/ProxyTest.php
@@ -14,12 +14,36 @@ class ProxyControllerProxyTest extends PHPUnit_Framework_TestCase
      */
     public function testSync()
     {
-        Config::$getClosure = function ($name) {
-            return [$name];
-        };
 
-        dd(Config::get('test'));
-        $sync = new Sync('<');
+        $sync = m::mock('sync')->makePartial();
+        $guzzle = m::mock('\GuzzleHttp\Client');
+        $url = "example.org";
+        $text = "lorem ipsum";
+
+        $sync->shouldReceive('getConfig')->andReturn(array('servers' => ['test' => ['url' => $url]]));
+        $sync->shouldReceive('getGuzzle')->andReturn($guzzle);
+
+        $guzzle->shouldReceive('post')->with($url, m::on(function ($data) use ($guzzle, $text) {
+            $this->assertEquals('', $data['body']);
+            $guzzle->shouldReceive('getBody')->andReturn($text);
+            return true;
+        }))->andReturn($guzzle);
+
+        $result = $sync->sync('test');
+        $this->assertEquals($text, $result);
+    }
+
+    /**
+     * Test if an abort is done with a 404 when the sync does not exist
+     */
+    public function testAbort()
+    {
+        $sync = m::mock('sync')->makePartial();
+        $app = m::mock('app');
+
+        $sync->shouldReceive('getConfig')->andReturn(array('servers' => []));
+        $sync->shouldReceive('getApp')->andReturn($app);
+        $app->shouldReceive('abort')->with(404);
         $sync->sync('test');
     }
 }

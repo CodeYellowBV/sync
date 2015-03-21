c2c22eb5179577df587e6824edb050a9c0759063
Stefan <stefan@codeyellow.nl>
2015-03-21 10:46:14 +0100

    Added an adapter for laravel4 logging to make it PSR3 compatible

---
+++ src/CodeYellow/Sync/Logger/Laravel4Adapter.php
@@ -0,0 +1,138 @@
+<?php
+namespace CodeYellow\Sync\Logger;
+
+use Illuminate\Log\Writer;
+/**
+ * Adapter to make the laravel 4 logger PSR3 compatible
+ */
+class Laravel4Adapter implements \Psr\Log\LoggerInterface
+{
+    // The laravel logger
+    private $laravelLogger;
+
+    public function __construct(
+        Writer $laravelLogger
+    ) {
+        $this->laravelLogger = $laravelLogger;
+    }
+
+       /**
+     * System is unusable.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function emergency($message, array $context = array())
+    {
+        $this->laravelLogger->emergency($message);
+    }
+
+    /**
+     * Action must be taken immediately.
+     *
+     * Example: Entire website down, database unavailable, etc. This should
+     * trigger the SMS alerts and wake you up.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function alert($message, array $context = array())
+    {
+        $this->laravelLogger->emergency($message);
+    }
+
+    /**
+     * Critical conditions.
+     *
+     * Example: Application component unavailable, unexpected exception.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function critical($message, array $context = array())
+    {
+        $this->laravelLogger->critical($message);
+    }
+
+    /**
+     * Runtime errors that do not require immediate action but should typically
+     * be logged and monitored.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function error($message, array $context = array())
+    {
+        $this->laravelLogger->error($message);
+    }
+
+    /**
+     * Exceptional occurrences that are not errors.
+     *
+     * Example: Use of deprecated APIs, poor use of an API, undesirable things
+     * that are not necessarily wrong.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function warning($message, array $context = array())
+    {
+        $this->laravelLogger->warning($message);
+    }
+
+    /**
+     * Normal but significant events.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function notice($message, array $context = array())
+    {
+        $this->laravelLogger->notice($message);
+    }
+
+    /**
+     * Interesting events.
+     *
+     * Example: User logs in, SQL logs.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function info($message, array $context = array())
+    {
+        $this->laravelLogger->info($message);
+    }
+
+    /**
+     * Detailed debug information.
+     *
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function debug($message, array $context = array())
+    {
+        $this->laravelLogger->debug($message);
+    }
+
+    /**
+     * Logs with an arbitrary level.
+     *
+     * @param mixed $level
+     * @param string $message
+     * @param array $context
+     * @return null
+     */
+    public function log($level, $message, array $context = array())
+    {
+        $this->{$level}($message);
+    }
+}
